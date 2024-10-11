411a3d41ea2fa966e6bd953fde3e32591217d11c
ronalynbaniagas <baniagasronalyn@gmail.com>
2024-10-11 10:37:37 +0800

    Add files via upload

---
+++ SwitchStatement.js
@@ -0,0 +1,15 @@
+// SWITCH STATEMENT
+let day = "Tuesday";
+switch (day) {
+    case "Monday":
+        console.log("It is Monday");
+        break;
+    case "Tuesday":
+        console.log("It is Tuesday");
+        break;
+        default:
+    console.log("It is neither Monday or Tuesday or other day");
+}
+let week = ("YOUR TASK FOR THIS IS DAY: FINISH YOUR ASSIGNMENT");
+console.log(week);
+
